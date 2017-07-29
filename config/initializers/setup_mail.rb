ae644caaca9cf54848c3336f320813bf57e87f41
Oscar Luza <onluza@gmail.com>
2017-07-28 19:05:04 -0500

    added gems for testing and style for devise alerts

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => 'smtp.gmail.com',
     :port                 => 587,
     :user_name            => 'no.reply.contacts.app@gmail.com',
     :password             => 'Wasd0123',
     :authentication       => :plain,
     :enable_starttls_auto => true,
     :domain               => 'domain.com'
-
 }
